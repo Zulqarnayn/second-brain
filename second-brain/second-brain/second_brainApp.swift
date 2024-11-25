367608ef28cd99f16432727e28a05ecac1b69a8f
Asif Mujtaba <asif.mujtaba@tigerit.com>
2024-11-25 16:20:00 +0600

    Initial commit: Project setup and configuration

---
+++ second-brain/second-brain/second_brainApp.swift
@@ -0,0 +1,17 @@
+//
+//  second_brainApp.swift
+//  second-brain
+//
+//  Created by Asif Mujtaba on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct second_brainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
